368800f8403c017f864df6c671613244034ee685
WHITERABBlT <143911024+WHITERABBlT@users.noreply.github.com>
2025-10-08 16:23:05 +0200

    Update speech2text_app.py

--- speech2text_app.py
+++ speech2text_app.py
@@ -17,10 +17,11 @@ from ibm_watson_machine_learning.foundation_models import Model
 my_credentials = {
     "url"    : "https://us-south.ml.cloud.ibm.com"
 }
+
 params = {
         GenParams.MAX_NEW_TOKENS: 700, # The maximum number of tokens that the model can generate in a single run.
         GenParams.TEMPERATURE: 0.1,   # A parameter that controls the randomness of the token generation. A lower value makes the generation more deterministic, while a higher value introduces more randomness.
-    }
+}
 
 LLAMA3_model = Model(model_id="meta-llama/llama-3-2-11b-vision-instruct", credentials=my_credentials, params=params, project_id="skills-network")
 
